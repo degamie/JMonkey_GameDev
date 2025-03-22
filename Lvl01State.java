8de70e21158f92d1c02573ce1cf33454f6d80182
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-22 09:44:37 +0530

    Add files via upload

---
+++ Lvl01State.java
@@ -0,0 +1,12 @@
+package MainApp;
+
+import org.w3c.dom.Node;
+
+public class Lvl01State extends AbstractAppState{
+	private final Node rootNode=new Node();
+	public String Lvl01State(SimpleApplication) {
+		rootNode=app.getRootNOde();
+		 
+	}
+
+}
