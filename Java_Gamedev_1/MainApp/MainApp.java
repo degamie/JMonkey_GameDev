f3768a9fddd986f5ff0ce594fd2e6d99b938ff31
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-23 21:22:48 +0530

    Add files via upload

--- Java_Gamedev_1/MainApp/MainApp.java
+++ Java_Gamedev_1/MainApp/MainApp.java
@@ -4,12 +4,22 @@ import com.jme3.app.SimpleApplication;
 import com.jme3.system.AppSettings;
 
 public class MainApp extends SimpleApplication{
+	public MainApp() {//Default Constructor Declare within its Param
+		this(new statsAppState(),new FlyCamAppState(),new AudioListenerState(),new DebugKeysAppStaate(),new ConstantVerifierState());
+	}
 	public final static void Main(String [] args) {
 		MainApp main=new MainApp();
 		AppSettings settings=new AppSettings(true);
+		main.start();
+		//Settings parameters Declare
 		settings.setTitle("GameDev");
 		main.setSettings(settings);
-		main.start();
+		settings.setWidth(1080);//Width parameters Declare
+		settings.setHeight(720);//Height parameters Declare
+		settings.setCenterWindow(false);//Central Window parameters Declare
+		settings.setWindowXPosition(0);//XPos parameters Declare
+		settings.setWindowYPosition(0);//YPos parameters Declare
+	
 	}
 
 	@Override
