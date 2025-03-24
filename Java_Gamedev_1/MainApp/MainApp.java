0f1b0a103c4195caf00d9021451df1dadca9be88
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-24 23:37:24 +0530

    Add files via upload

--- Java_Gamedev_1/MainApp/MainApp.java
+++ Java_Gamedev_1/MainApp/MainApp.java
@@ -1,6 +1,9 @@
 package MainApp;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Geometry;
 import com.jme3.system.AppSettings;
 
 public class MainApp extends SimpleApplication{
@@ -19,7 +22,12 @@ public class MainApp extends SimpleApplication{
 		settings.setCenterWindow(false);//Central Window parameters Declare
 		settings.setWindowXPosition(0);//XPos parameters Declare
 		settings.setWindowYPosition(0);//YPos parameters Declare
-	
+		
+		Geometry geometry=new Geometry("Cone",mesh);//Cone's gemotery Declare
+		Material material=new Material(assetManager,"Common/MatDefs/Mat_tex.j3nd");//Material 's Declare
+		material.setColor("Color",ColorRGBA.Magenta);//Materials's Color Declare
+		material.setMaterial(material);//Material val declare
+		rootNode.geometry(geometry);//RootNode geometry Declare
 	}
 
 	@Override
