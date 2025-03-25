d7c5818c5faa0a62079de27119df7fdf47865a35
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-25 23:25:30 +0530

    Add files via upload

--- Java_Gamedev_1/MainApp/MainApp.java
+++ Java_Gamedev_1/MainApp/MainApp.java
@@ -1,9 +1,11 @@
 package MainApp;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.input.controls.ActionListener;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.system.AppSettings;
 
 public class MainApp extends SimpleApplication{
@@ -28,6 +30,17 @@ public class MainApp extends SimpleApplication{
 		material.setColor("Color",ColorRGBA.Magenta);//Materials's Color Declare
 		material.setMaterial(material);//Material val declare
 		rootNode.geometry(geometry);//RootNode geometry Declare
+		Node rotation=new Node("rotation");//Rotation Obj declare
+		rotation.rotate(.2f,.4f,.6f);//rotating the Obj 
+		rotation.attachChild(geometry);//rotating the Obj's geometry
+		rotation.attachChild(rotation);///rotating the Obj's axis
+		
+	}
+	ActionListener actionlistioner=new ActionListener() {//Axis Listner
+		@Override
+		public void OnAction(String name,Boolean isPressed,float tpf) {//Adding Action
+			if(name.equals("Rotate")&& !isPressed)rotationEnabled=!rotationEnabled;//Enabling Obj Rotation
+		}
 	}
 
 	@Override
