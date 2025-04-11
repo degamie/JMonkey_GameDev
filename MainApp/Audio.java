ecb01636cf6fc883be9256b396f8a8f1ae7a1c7d
Degamie_sign <degamiesign@gmail.com>
2025-04-11 23:14:07 +0530

    Thirteenth Commit

---
+++ MainApp/Audio.java
@@ -0,0 +1,12 @@
+package MainApp;
+
+public class Audio {//Class Declare
+	public Audio audio;//Audio Obj declare
+	public Boolean setAudio(Boolean isAudioPlayable) {//Checking the Audio
+		return isAudioPlayable=true;
+	}
+	public  String playAudio() {//Playing the Audio  
+		public audio.play();
+	}
+
+}
