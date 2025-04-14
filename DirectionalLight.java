8b36c72ec03945db36eb090ed594af1d47bc8187
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-04-14 23:22:35 +0530

    Add files via upload

---
+++ DirectionalLight.java
@@ -0,0 +1,13 @@
+package Light;
+
+public class DirectionalLight {//DirectionalLight Class Declare
+	RootNode rootNode=new RootNode();//Obj Declare
+	DirectionalLight sun=new DirectionalLight();//DirectionalLight's Obj Declare
+	sun.setDirection(new Vector3f(1,0,-2).normalizeLocal());//setDirection Sunlight's Dire
+	sun.setColor(ColorRGBA.Orange);//Sun Color Declare
+	rootNode.addingLight(sun);//Adding Light to Sun
+	
+	public DirectionalLight(DirectionalLight sun) {
+		this.sun=sun;
+	}
+}
---
+++ SpotLight.java
@@ -0,0 +1,10 @@
+package Light;
+
+import com.jme3.light.Light;
+
+public class SpotLight extends Light{
+	public SpotLight spotlight;
+	public SpotLight getspotlight(SpotLight spotlight) {
+		return spotlight;
+	}
+}
