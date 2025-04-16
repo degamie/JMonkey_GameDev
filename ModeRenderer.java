972919b916510630523c9478a200665a04d22b30
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-04-16 23:18:18 +0530

    Add files via upload

---
+++ ModeRenderer.java
@@ -0,0 +1,19 @@
+package MainApp;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.material.Material;
+
+public class ModeRenderer {//ModeRenderer Class declare
+	AssetManager assetManager=new AssetManager();//AssetManager obj declare
+	Spatial spatial=assetManager.loadModel("Models/home.j3e"); //Loading Model 
+	Material mat=new Material(assetManager,"Lighting.j3d");//Material Obj declare
+	public String getMaterial(Material mat) {//fetching Material
+		return mat;
+	}
+	mat.setBoolean("UserMaterial Colors",true);//Setting Boolean Expression
+	mat.setColor("Ambient"ColorRGBA.Blue);//Setting The Material's Col
+	mat.setColor("Diffuse",ColorRGBA.Blue);//Diffusing Material
+	spatial.setMaterial(mat);//Spatial's Material  set declare
+	
+
+}
