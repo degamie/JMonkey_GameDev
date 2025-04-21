28d1d8cf0b23bc999dc3bd9d465436e72d066c9c
Degamie_sign <degamiesign@gmail.com>
2025-04-21 23:01:31 +0530

    Eigteenth Commit

--- MainApp/ModeRenderer.java
+++ MainApp/ModeRenderer.java
@@ -2,6 +2,8 @@ package MainApp;
 
 import com.jme3.asset.AssetManager;
 import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Spatial;
 
 public class ModeRenderer {//ModeRenderer Class declare
 	AssetManager assetManager=new AssetManager();//AssetManager obj declare
@@ -15,9 +17,11 @@ public class ModeRenderer {//ModeRenderer Class declare
 	}
 
 	mat.setBoolean("UserMaterial Colors",true);//Setting Boolean Expression
-	mat.setColor("Ambient"ColorRGBA.Blue);//Setting The Material's Col
+	ColorRGBA BlueColor=mat.setColor("Ambient"ColorRGBA.Blue);//Setting The Material's Col
 	mat.setColor("Diffuse",ColorRGBA.Blue);//Diffusing Material
 	spatial.setMaterial(mat);//Spatial's Material  set declare
-	
+	public String getBlueColor(ColorRGBA BlueColor) {//Fetching BlueColor 
+		return BlueColor;
+	}
 
 }
