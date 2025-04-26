b9adee7a0e477a5d86eda3b394d257154ddbfa2e
Degamie_sign <degamiesign@gmail.com>
2025-04-26 16:11:40 +0530

    Twenty First Commit

--- MainApp/LevelManagment.java
+++ MainApp/LevelManagment.java
@@ -3,6 +3,9 @@ package MainApp;
 import com.jme3.app.SimpleApplication;
 
 public class LevelManagment extends SimpleApplication {
+	public final Node rootNode;
+	public Node localRootNode;
+	
 	ArrayList<LevelManagingClass>lvlMngmt=new ArrayList<>();
 	lvlMngmt.add(lvel1);
 	lvlMngmt.add(lvel2);
@@ -13,5 +16,9 @@ public class LevelManagment extends SimpleApplication {
 	lvlMngmt.get(lvl).load();
 	public String getLvel(ArrayList<LevelManagingClass>lvlMngmt,int lvl) {
 		return "Level:"+lvlMngmt+lvl;
+	}
+	public String initialize(AppState manager,Application app) {
+		super.initialize(Statemanager app);
+		rootNode.attachChild(localRootNode);
 	}
 }
--- MainApp/SceneCaster.java
+++
@@ -1,8 +0,0 @@
-package MainApp;
-
-public class SceneCaster {
-	public ScenePerformer scenePerformer;
-	public String getScenePerformer(ScenePerformer scenePerformer) {
-		return scenePerformer;
-	}
-}
