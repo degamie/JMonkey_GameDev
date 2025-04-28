9688b96fabe50f3e109b2877f58ce884dd5b60b2
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-04-28 23:08:56 +0530

    Add files via upload

--- Java_Gamedev_1/MainApp/LevelManagment.java
+++ Java_Gamedev_1/MainApp/LevelManagment.java
@@ -3,6 +3,9 @@ package MainApp;
 import com.jme3.app.SimpleApplication;
 
 public class LevelManagment extends SimpleApplication {
+	public final Node rootNode;
+	public Node localRootNode;
+	
 	ArrayList<LevelManagingClass>lvlMngmt=new ArrayList<>();
 	lvlMngmt.add(lvel1);
 	lvlMngmt.add(lvel2);
@@ -11,7 +14,14 @@ public class LevelManagment extends SimpleApplication {
 	lvlMngmt.add(lvel5);
 	int lvl=0;
 	lvlMngmt.get(lvl).load();
+	public String setLvel(int lvl) {//binding Level
+		this.lvl=lvl;
+	}
 	public String getLvel(ArrayList<LevelManagingClass>lvlMngmt,int lvl) {
 		return "Level:"+lvlMngmt+lvl;
+	}
+	public String initialize(AppState manager,Application app) {
+		super.initialize(Statemanager app);
+		rootNode.attachChild(localRootNode);
 	}
 }
