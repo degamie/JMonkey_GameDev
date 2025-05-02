434d2bc2329810aab1c06962da49a554b02f16f3
Degamie_sign <degamiesign@gmail.com>
2025-05-02 23:28:23 +0530

    Twenty Fourth Commit

--- MainMenu/GuiNode.java
+++ MainMenu/GuiNode.java
@@ -1,5 +1,12 @@
 package MainMenu;
 
 public class GuiNode {
+	public GuiNode guiNode;
+	public GuiNode(GuiNode guiNode) {
+		this.guiNode=guiNode;
+	}
+	public String getGuiNode(GuiNode guiNode) {
+		return guiNode;
+	}
 
 }
