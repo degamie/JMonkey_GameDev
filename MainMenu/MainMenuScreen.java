577e5fd39094d7f8bd076ea3f0a60cf925809be1
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-05-28 09:20:42 +0530

    Add files via upload

--- MainMenu/MainMenuScreen.java
+++ MainMenu/MainMenuScreen.java
@@ -8,15 +8,29 @@ import de.lessvoid.nifty.screen.Screen;
 public class MainMenuScreen extends SimpleApplication {
 	Screen screen=new Screen(this);
 	GuiNode guinode=new GuiNode(this);
+	public String getGuiNode(GuiNode guiNode) {
+		return guiNode;
+	}
+	public Screen display=new Screen(guiNode);
+	public  Menu elem1=new Menu(screen,new Vector2(0,0);
+	public  Menu elem2=new Menu(screen,new Vector2(0,0);
+	public  Menu elem3=new Menu(screen,new Vector2(0,0);
+	
+	public  Menu elem4=new Menu(screen,new Vector2(0,0);
+}
+	public int width=0;public int Height=0;
+
 	guiNode.addControl(screen);
-	Menu subElement=new Menu(screen,new Vector2(0,0),
-			false){
+//	Menu subElement=new Menu(screen,new Vector2(0,0),
+//			false){
 				@Override
 				public void OnMenuItemClicked(String indx,Object val,Boolean isToggled) {
-					subElement.addMenuItem("Menu Screen 01", null, null);
-					subElement.addMenuItem("Menu Screen 02", null, null);
-					subElement.addMenuItem("Menu Screen 03", null, null);
-					subElement.addMenuItem("Menu Screen 04", null, null);
+					public Menu subElement=new Menu(screen,new Vector2(0,0);
+					subElement=elem1+elem2+elem3+elem4;
+					elem1.addMenuItem("Menu Screen 01", null, null);
+					elem2.addMenuItem("Menu Screen 02", null, null);
+					elem3.addMenuItem("Menu Screen 03", null, null);
+					elem4.addMenuItem("Menu Screen 04", null, null);
 				screen.addElement(subElement);
 				}
 			b.setText("Show Menu");
@@ -26,9 +40,28 @@ public class MainMenuScreen extends SimpleApplication {
 		return screen;
 	}
 
-	@Override
-	public void simpleInitApp() {
-		// TODO Auto-generated method stub
+	public String simpleInitApp(Screen display ,Screen screen,GuiNode guinode,int width,int Height,) {
+		 if(screen==null)return null;
+		 while(guiNode!=null) {
+			 if(width==720 && height==1080) {
+				 display=screen.addElement(elem1);
+				 
+			 }
+			 if(width==1080 && height==1440) {
+				 display=screen.addElement(elem2);
+				 
+			 }
+			 if(width==1440 && height==2140) {
+				 display=screen.addElement(elem3);
+				 
+			 }
+			 if(width==2140 && height==4200) {
+				 display=screen.addElement(elem4);
+				 
+			 }
+		 }
+		 return display;
+	
 		
-	}}
+	}
 //extends SimpleAp
