4f78e0cf48f4207a9ec4ee61f495ada25abf0c08
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-06-06 23:34:03 +0530

    Add files via upload

---
+++ MainApp/Classes/Audio.java
@@ -0,0 +1,39 @@
+package MainApp;
+
+public class Audio {//Class Declare
+	public float speed=0.0f;	//speed initailize
+	public Audio audio;//Audio Obj declare
+	public OnInit(Node audio){
+		if(audio===milll)
+	}
+	public Audio(Audio audio,float Speed) {
+		this.audio=audio;
+		this.speed=Speed;
+	}
+	public Audio fastForward(float speed,Audio audio) {//fastForward funct declare 
+		if(speed==0)return 0;//Base cond
+		while(speed!=0) {//Iterating Through speed
+		else if(speed>0) {speed+=1.75;}//Checking Speed >0 aND incr 1.75
+			else speed=0;}//Speed initial val declare
+			return audio.play(speed);//Printing Output
+	}
+
+	public String getAudio(Audio audio) {
+		return audio;
+	}
+	public Boolean setAudio(Boolean isAudioPlayable) {//Checking the Audio
+		return isAudioPlayable=true;
+	}
+	public  String playAudio() {//Playing the Audio  
+		public audio.play();
+	}
+	public Integer OnStart(Audio audio,float speed) {
+		if(speed==0)return speed;
+		while(speed!=0) {
+			if(speed>0) {
+				speed+=playAudio();
+			}else speed=0;
+		}return speed;
+	}
+
+}
