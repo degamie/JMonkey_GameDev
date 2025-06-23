a4934094d7a3a25d7a3f3dc5c07e2d1fe0e5eb3c
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-06-23 22:34:23 +0530

    Add files via upload

---
+++ MainApp/ModelRenderer.java
@@ -0,0 +1,30 @@
+package MainApp;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Spatial;
+
+public class ModelRenderer {//ModeRenderer Class declare
+	AssetManager assetManager=new AssetManager();//AssetManager obj declare
+	Spatial spatial=assetManager.loadModel("Models/home.j3e"); //Loading Model 
+	Material mat=new Material(assetManager,"Lighting.j3d");//Material Obj declare
+	public String setModel(Model model) {//fetching Model
+		return ModelRenderer.findAll(model);//Printing Model
+	}
+	public String getModel(Model model) {//fetching Model
+		return model;//Printing Model
+	}
+	public String getMaterial(Material mat) {//fetching Material
+		return mat;
+	}
+
+	mat.setBoolean("UserMaterial Colors",true);//Setting Boolean Expression
+	ColorRGBA BlueColor=mat.setColor("Ambient"ColorRGBA.Blue);//Setting The Material's Col
+	mat.setColor("Diffuse",ColorRGBA.Blue);//Diffusing Material
+	spatial.setMaterial(mat);//Spatial's Material  set declare
+	public String getBlueColor(ColorRGBA BlueColor) {//Fetching BlueColor 
+		return BlueColor;
+	}
+
+}
