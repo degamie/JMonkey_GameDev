691bb735d8793228169c5f889b3348121b20e511
Degamie_sign <degamiesign@gmail.com>
2025-06-25 09:17:13 +0530

    Fourty Fifth Commit

--- Java_Gamedev_1/MainApp/ScenePerformer.java
+++ Java_Gamedev_1/MainApp/ScenePerformer.java
@@ -3,6 +3,9 @@ package MainApp;
 import com.jme3.scene.plugins.ogre.SceneLoader;
 
 public class ScenePerformer extends MainApp {
+	ScenePerformer( SceneLoader scenePerformer){
+		this.scenePerformer=scenePerformer;
+	}
 	public SceneLoader scenePerformer=new SceneLoader();
 	public void setScenePerformer(SceneLoader scenePerformer) {this.scenePerformer=scenePerformer;}//Binding ScenePerformer
 
