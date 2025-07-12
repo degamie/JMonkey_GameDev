429b706ed200edc93df87ca126d1417059603f84
Degamie_sign <degamiesign@gmail.com>
2025-07-12 13:23:46 +0530

    Fiftyth Commit

--- Java_Gamedev_1/MainApp/LevelManagment.java
+++ Java_Gamedev_1/MainApp/LevelManagment.java
@@ -14,6 +14,9 @@ public class LevelManagment extends SimpleApplication {
 		return lvlMngmt.saveAll(rootNode);//rootNode's Fetching
 	}
 	public Node localRootNode;
+	public String getLocalRootNOde(ArrayList<LevelManagingClass>lvlMngmt,Node localrootNode) {
+		return lvlMngmt.saveAll(localrootNode);
+	}
 	
 	LevelvManagment (Node rootNode,Node localRootNode,ArrayList<LevelManagingClass>lvlMngmt,int lvl){
 //Param Constr
