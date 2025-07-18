7345752bf77651fba14a7492b12ac30031bb25a6
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-07-18 23:02:32 +0530

    Add files via upload

---
+++ MainApp/Classes/ScenePerformer.java
@@ -0,0 +1,17 @@
+package MainApp;
+
+import com.jme3.scene.plugins.ogre.SceneLoader;
+
+public class ScenePerformer extends MainApp {
+	public ScenePerformer scenePerformer;
+	ScenePerformer( SceneLoader scenePerformer){
+		this.scenePerformer=scenePerformer;
+	}
+	public SceneLoader scenePerformer=new SceneLoader();
+	public void setScenePerformer(SceneLoader scenePerformer) {this.scenePerformer=scenePerformer;}//Binding ScenePerformer
+
+	public String getScenePerformer(SceneLoader scenePerformer) {
+		return scenePerformer;
+	}
+
+}
