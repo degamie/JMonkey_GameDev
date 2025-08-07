a8a59f84d5ae051bdfc7fbf210b150e8774b05d3
Degamie_sign <degamiesign@gmail.com>
2025-08-07 20:46:39 +0530

    Fifty Two Commit

---
+++ Java_Gamedev_1/MainApp/NumMulDiv_Game/old/Light/DirectionalLight.java
@@ -0,0 +1,21 @@
+package Light;
+
+public class DirectionalLight {//DirectionalLight Class Declare
+	RootNode rootNode=new RootNode();//Obj Declare
+	DirectionalLight sun=new DirectionalLight(rootNode, sun);//DirectionalLight's Obj Declare
+	DirectionalLight(RootNode rootNode,DirectionalLight sun){//param Constructor
+		this.rootNode=rootNode;
+		this.sun=sun;
+	}
+	
+	sun.setDirection(new Vector3f(1,0,-2).normalizeLocal());//setDirection Sunlight's Dire
+	sun.getDirection(new Vector3f(1,0,-2).normalizeLocal());//Fetching SunLight's Direction
+	//Implemented(07/07/2025)
+	sun.getColor(ColorRGBA.Yellow);//Sun Color's Fetch
+	
+	sun.setColor(ColorRGBA.Orange);//Sun Color Declare
+
+	rootNode.addingLight(sun);//Adding Light to Sun
+	
+	
+}
